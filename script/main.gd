d2b7c7eefb5abeb17d4819f2e43f0b2d6adbe57c
Marshall <otaga203@gmail.com>
2025-04-19 08:57:55 -0300

    Placing desk.

---
+++ script/main.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+func _init() -> void:
+	pass
+
+
+func _ready() -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
